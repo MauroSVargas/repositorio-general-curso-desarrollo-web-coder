33a81c78435786d33da09b81bbf2b40e7b3a4ef7
mauro_vargas <mauroseba2000@gmail.com>
2022-08-21 18:42:39 -0300

    php 2.0

--- form.php
+++ form.php
@@ -17,9 +17,9 @@ $mensaje. ="su telefono es" . $celular . ",\r\n";
 
 
 //----------RADIO BUTTONS----------//
-echo "seleccione su género: "; echo $_POST['sexo'];
+echo "seleccione su género: "; echo $_POST['seleccione su género'];
 
-echo "cantidad: "; echo $_POST['cantidad'] '<p> y ha seleccionado' .$_POST['seleccione su género']. '</p>';
+echo "cantidad: "; echo $_POST['cantidad'] '<p> y ha seleccionado' .$_POST['cantidad']. '</p>';
 
 $mensaje .="enviado el " . date('d/m/Y', time());
 $destinatario = 'contacto.snipper7@gmail.com';
