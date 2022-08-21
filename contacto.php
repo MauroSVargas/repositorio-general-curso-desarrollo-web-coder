71891189b6556160d357b2a6bbfd8764a902c43e
mauro_vargas <mauroseba2000@gmail.com>
2022-08-21 18:14:54 -0300

    form funcional

---
+++ contacto.php
@@ -0,0 +1,36 @@
+<?php 
+
+//----------VARIABLES----------//
+$nombre= $_POST["nombre"];
+$apellido= $_POST["apellido"];
+$celular= $_POST["celular"];
+/*$hombre= $_POST["hombre"];
+$mujer= $_POST["mujer"];
+$binario= $_POST["no binario"];
+$clase1= $_POST["1"];
+$clase2= $_POST["2"];
+$clase3= $_POST["3"];*/
+
+//----------CUERPO DE MENSAJE----------//
+$mensaje= "este mensaje fue enviado por" . $nombre . $apellido . ",\r\n";
+$mensaje. ="su telefono es" . $celular . ",\r\n";
+
+
+//----------RADIO BUTTONS----------//
+if (isset($_POST['seleccione su género'])) {
+    echo '<p>es '.$_POST['seleccione su género'].'</p>' . ",\r\n";
+}
+
+echo "cantidad: "; echo $_POST['cantidad'] '<p> y ha seleccionado' .$_POST['seleccione su género']. '</p>';
+
+$mensaje .="enviado el " . date('d/m/Y', time());
+$destinatario = 'mauroseba2000@gmail.com';
+$asunto = 'enviado desde bypaulamakeup';
+
+mail($destinatario, $asunto, $mensaje, utf8_decode($mensaje), $header);
+
+header ('location:contacto.html');
+
+
+?>
+
